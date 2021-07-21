a737bb2736bd3e4b5dd6159b7a068615e0158bf4
Jazmín Patrón Galindo <jazmin@MacBook-Pro-de-Jazmin.local>
2021-07-21 16:16:24 -0500

    just import choice from module random, not all the module

--- touches/randoms.py
+++ touches/randoms.py
@@ -1,16 +1,15 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-import random 
+from random import choice
 
 def randomSelectChoice(driver, element):
     div_selection = driver.find_element(*element)
     options = div_selection.find_elements(By.TAG_NAME, 'option')[1:]
-    randomly_selection = random.choice(options).text
+    randomly_selection = choice(options).text
 
     select = Select(div_selection)
     select.select_by_visible_text(randomly_selection)
 
     print(randomly_selection)
     return randomly_selection
-    
