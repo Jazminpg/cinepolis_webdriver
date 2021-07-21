8e8c5e8479d8a60629c1ec0bd18f18d90fd82ff2
Jazmín Patrón Galindo <jazmin@MacBook-Pro-de-Jazmin.local>
2021-07-21 16:58:33 -0500

    minor changes in essential file

--- essential.py
+++ essential.py
@@ -3,9 +3,9 @@ from pages.home import HomePage
 
 if __name__ == "__main__":
     with webdriver.Chrome('./chromedriver.log') as driver:
-        home = HomePage(driver)
-        home.load()
-        home.city()
-        home.cine()
+        homePage = HomePage(driver)
+        homePage.load()
+        homePage.city()
+        homePage.cine()
         
 
