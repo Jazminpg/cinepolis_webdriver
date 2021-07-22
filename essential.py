5662317aa5ca5426b16b3897f19a21644c572898
Jazmín Patrón Galindo <jazmin@MacBook-Pro-de-Jazmin.local>
2021-07-22 08:37:06 -0500

    minor changes in main script

--- essential.py
+++ essential.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from pages.home import HomePage
 
-if __name__ == "__main__":
-    with webdriver.Chrome('./chromedriver.log') as driver:
-        homePage = HomePage(driver)
-        homePage.load()
-        homePage.city()
-        homePage.cine()
+with webdriver.Chrome('./chromedriver.log') as driver:
+    homePage = HomePage(driver)
+    homePage.load()
+    homePage.city()
+    homePage.cine()
         
 
